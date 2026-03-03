d1d820bd2a3c45280eb29538663c1b97fd4821f8
HoangNhatLongg <162867527+HoangNhatLongg@users.noreply.github.com>
2026-03-03 17:03:43 +0700

    Add files

---
+++ ProductService/Queries/GetProductsQuery.cs
@@ -0,0 +1,4 @@
+﻿using MediatR;
+using System.Collections.Generic;
+
+public record GetProductsQuery() : IRequest<List<string>>;
---
+++ ProductService/Queries/GetProductsQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class GetProductsQueryHandler
+    : IRequestHandler<GetProductsQuery, List<string>>
+{
+    public Task<List<string>> Handle(
+        GetProductsQuery request,
+        CancellationToken cancellationToken)
+    {
+        var products = new List<string>
+        {
+            "Life Insurance",
+            "Car Insurance",
+            "Health Insurance"
+        };
+
+        return Task.FromResult(products);
+    }
+}
